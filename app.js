bd2888c3e399b8f238c18f558a634f95a66b9113
crittenbach1 <crittenbach@gmail.com>
2019-02-09 10:20:32 -0500

    use node fs module to show string array of directory files

--- app.js
+++ app.js
@@ -4,15 +4,29 @@
 //
 // console.log(pathObj);
 
-const os = require('os');
+// **********************************
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+// const os = require('os');
+//
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
+//
+// //console.log('Total memory: ' + totalMemory);
+//
+// //Template string
+// //ES6 / ES2015 : ECMAScript 6
+//
+// console.log(`Total Memory: ${totalMemory}`);
+// console.log(`Free Memory: ${freeMemory}`);
 
-//console.log('Total memory: ' + totalMemory);
+// **********************************
 
-//Template string
-//ES6 / ES2015 : ECMAScript 6
+const fs = require('fs');
 
-console.log(`Total Memory: ${totalMemory}`);
-console.log(`Free Memory: ${freeMemory}`);
+const files = fs.readdirSync('./');
+console.log(files);
+
+fs.readdir('$', './', function(err, files) {
+  if (err) console.log('Error', err);
+  else console.log('Result', files);
+});
