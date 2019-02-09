31fb582fe7bf599e17f2c99a255204526c1fa541
crittenbach1 <crittenbach@gmail.com>
2019-02-09 10:11:32 -0500

    get OS info from node os module functions

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const path = require('path');
+// const path = require('path');
+//
+// var pathObj = path.parse(__filename);
+//
+// console.log(pathObj);
 
-var pathObj = path.parse(__filename);
+const os = require('os');
 
-console.log(pathObj);
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+//console.log('Total memory: ' + totalMemory);
+
+//Template string
+//ES6 / ES2015 : ECMAScript 6
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
