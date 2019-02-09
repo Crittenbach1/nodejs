83bc958ea52107a2f14903b83c00959e66a9ea89
crittenbach1 <crittenbach@gmail.com>
2019-02-09 11:34:12 -0500

    connect to server 3000 with http module

--- app.js
+++ app.js
@@ -33,12 +33,29 @@
 
 // **********************************
 
-const Logger = require('./logger');
-const logger = new Logger();
+// const Logger = require('./logger');
+// const logger = new Logger();
+//
+//
+//
+// // Register a listener
+// logger.on('messageLogged', (arg) => {
+//   console.log('Listener called', arg);
+// })
+//
+// logger.log('message');
+//
 
-// Register a listener
-logger.on('messageLogged', (arg) => {
-  console.log('Listener called', arg);
+// **********************************
+
+const http = require('http');
+
+const server = http.createServer();
+
+server.on('connection', (socket) => {
+  console.log('New connection...');
 })
 
-logger.log('message');
+server.listen(3000);
+
+console.log('Listening on port 3000...')
